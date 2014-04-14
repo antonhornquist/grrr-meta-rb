0b282d51f7ffea29b5510ec75a55fa3cc903b045
Anton Hörnquist <anton.hornquist@gmail.com>
2014-04-14 23:33:57 +0200

    Refactored to enable linode and windows code generation

--- grrr_meta.rb
+++ grrr_meta.rb
@@ -1,194 +1,217 @@
 require 'rsclass'
+require 'tmpdir'
 
-def grrr_meta_user_say_yes?
-	["y", ""].include?(STDIN.gets.chomp.downcase)
-end
-
-def grrr_meta_get_user_number_in_range(range)
-	print "[#{range.min}-#{range.max}] "
-	number = STDIN.gets.chomp.to_i
-	exit unless range.include? number 
-	number
-end
-
-def grrr_meta_get_rootdir
-	File.expand_path(File.dirname(__FILE__))
-end
-
-def grrr_meta_get_repodir
-	"#{grrr_meta_get_rootdir}/def"
-end
-
-def grrr_meta_get_defpath(defname)
-	"#{grrr_meta_get_repodir}/#{defname}.rsclass.rb"
-end
-
-def grrr_meta_parse_def(filename)
-	name, type, format = File.basename(filename).split "."
-	file = {
-		:source => {
-			:name => name,
-			:type => type,
-			:format => format
-		},
-		:content => File.read(filename)
-	}
-	parse_file(file)
-end
-
-def grrr_meta_parse_def_arg(defname)
-	filename = grrr_meta_get_defpath(defname)
-	if not File.exists?(filename)
-		entries = Dir.entries(grrr_meta_get_repodir).
-			reject{|p|%w{. ..}.include?(p)}.
-			map {|p|p.split(".")[0]}.
-			select{|p|/#{defname}/ =~ p}
-
-		if entries.size == 0
-			puts "Argument matched no definition in repository."
-			exit
-		else
-			if entries.size == 1
-				single_match = entries.first
-				print "You meant \"#{single_match}\", right? [Yn] "
-				exit unless grrr_meta_user_say_yes?
-				filename = grrr_meta_get_defpath(single_match)
+module GrrrMeta
+	class <<self
+		def execute(operation, defname)
+			ensure_editor_environment_variable_is_set
+	
+			repo_entry = parse_def_arg(defname)
+			rest_of_repo = []
+			
+			case operation
+			when "generate"
+				grrr_meta_generate(repo_entry, rest_of_repo, [:rb_stub, :sc_stub]) # TODO: refactor rsclass :: generate and remove ugly prefix here
+			when "generate-rb"
+				grrr_meta_generate(repo_entry, rest_of_repo, [:rb_stub])
+			when "generate-sc"
+				grrr_meta_generate(repo_entry, rest_of_repo, [:sc_stub])
+			when "workcopyedit-rb"
+				workcopyedit_one(repo_entry, rest_of_repo, :rb_stub)
+			when "workcopyedit-sc"
+				workcopyedit_one(repo_entry, rest_of_repo, :sc_stub)
+			when "workcopyedit-both"
+				workcopyedit_both(repo_entry, rest_of_repo)
+			when "workcopyedit-sidebyside"
+				workcopyedit_sidebyside(repo_entry, rest_of_repo)
+			when "defedit"
+				defedit(repo_entry)
 			else
-				puts "Several matches in repository. Choose:"
-				entries.each_with_index { |item, i| puts "#{i+1}. #{item}" }
-				filename = grrr_meta_get_defpath(
-					entries[grrr_meta_get_user_number_in_range((1..entries.size))-1]
-				)
+				raise "unknown operation #{operation}"
 			end
 		end
-	end
 
-	grrr_meta_parse_def(filename)
-end
-
-def grrr_meta_write_n_diff(stub)
-	dest = stub[:destination]
-	rootdir = grrr_meta_get_rootdir
-	relpath = "#{dest[:directory]}/#{dest[:filename]}"
-	gen_fname = "#{rootdir}/tmp/#{relpath}"
-	workcopy_fname = "#{grrr_meta_get_workcopy_path(rootdir, dest[:directory])}/#{dest[:filename]}"
-	File.open("#{gen_fname}", 'w') {|f| f.write(stub[:content])}
-	print "file \"#{relpath}\" generated. diff with workcopy? [Yn] "
-	system("#{ENV["EDITOR"]} -d #{gen_fname} #{workcopy_fname}") if grrr_meta_user_say_yes?
-end
-
-def grrr_meta_get_workcopy_path(rootdir, destination_filetype)
-	path = rootdir + "\\" + destination_filetype
-	if File.directory?(path)
-		path
-	elsif File.file?(path)
-		path_in_file = IO.read(path).chomp
-		if File.directory?(path_in_file)
-			path_in_file
+		def print_usage
+			operations = [
+				"generate",
+				"generate-rb",
+				"generate-sc",
+				"workcopyedit-rb",
+				"workcopyedit-sc",
+				"workcopyedit-both",
+				"workcopyedit-sidebyside",
+				"defedit"
+			]
+			puts "usage: ruby #{__FILE__} operation [defname]"
+			puts "	operation:"
+			puts operations.map { |operation| "\t\t#{operation}" }.join("\n")
 		end
-	else
-		raise "File #{path} does not exist"
-	end or raise "Unable to determine work directory"
-end
 
-def grrr_meta_get_relpath(repo_entry, rest_of_repo, dest_type)
-	stub = generate(repo_entry, rest_of_repo, dest_type)
-	dest = stub[:destination]
-	"#{dest[:directory]}/#{dest[:filename]}"
-end
+		def ensure_editor_environment_variable_is_set
+			raise "EDITOR environment variable is not set" unless ENV["EDITOR"]
+		end
 
-def grrr_meta_get_rb_and_sc_relpaths(repo_entry, rest_of_repo)
-	rb_relpath = grrr_meta_get_relpath(repo_entry, rest_of_repo, :rb_stub)
-	sc_relpath = grrr_meta_get_relpath(repo_entry, rest_of_repo, :sc_stub)
-	[rb_relpath, sc_relpath]
-end
+		def grrr_meta_generate(repo_entry, rest_of_repo, dest_types)
+			dest_types.each do |dest_type|
+				stub = generate(repo_entry, rest_of_repo, dest_type)
+				write_n_diff(stub)
+			end
+		end
+		
+		def workcopyedit_one(repo_entry, rest_of_repo, dest_type)
+			path = get_workcopy_file_path_from_repo(repo_entry, rest_of_repo, dest_type)
+			print "\"#{ENV["EDITOR"]} #{File.basename(path)}\"? [Yn] "
+			system("#{ENV["EDITOR"]} #{path}") if user_say_yes?
+		end
+		
+		def workcopyedit_both(repo_entry, rest_of_repo)
+			rb_path, sc_path = get_rb_and_sc_workcopy_file_paths_from_repo(repo_entry, rest_of_repo)
+			print "\"#{ENV["EDITOR"]} #{[rb_path, sc_path].map {|p|File.basename(p)}.join(" ")}\"? [Yn] "
+			system("#{ENV["EDITOR"]} #{[rb_path, sc_path].join(" ")}") if user_say_yes?
+		end
+		
+		def workcopyedit_sidebyside(repo_entry, rest_of_repo)
+			rb_path, sc_path = get_rb_and_sc_workcopy_file_paths_from_repo(repo_entry, rest_of_repo)
+			print "\"#{ENV["EDITOR"]} -O2 #{[rb_path, sc_path].map {|p|File.basename(p)}.join(" ")}\"? [Yn] "
+			system("#{ENV["EDITOR"]} -O2 #{[rb_path, sc_path].join(" ")}") if user_say_yes?
+		end
+		
+		def defedit(repo_entry)
+			defpath = get_defpath(repo_entry["source"]["name"])
+			print "\"#{ENV["EDITOR"]} #{File.basename(defpath)}\"? [Yn] "
+			system("#{ENV["EDITOR"]} #{defpath}") if user_say_yes?
+		end
+		
+		def get_rootdir
+			File.expand_path(File.dirname(__FILE__))
+		end
+		
+		def get_repodir
+			File.join(get_rootdir, "def")
+		end
+		
+		def get_defpath(defname)
+			File.join(get_repodir, "#{defname}.rsclass.rb")
+		end
+		
+		def parse_def(filename)
+			name, type, format = File.basename(filename).split "."
+			file = {
+				:source => {
+					:name => name,
+					:type => type,
+					:format => format
+				},
+				:content => File.read(filename)
+			}
+			parse_file(file)
+		end
+		
+		def parse_def_arg(defname)
+			filename = get_defpath(defname)
+			if not File.exists?(filename)
+				entries = Dir.entries(get_repodir).
+					reject{|p|%w{. ..}.include?(p)}.
+					map {|p|p.split(".")[0]}.
+					select{|p|/#{defname}/ =~ p}
+		
+				if entries.size == 0
+					puts "Argument matched no definition in repository."
+					exit
+				else
+					if entries.size == 1
+						single_match = entries.first
+						print "You meant \"#{single_match}\", right? [Yn] "
+						exit unless user_say_yes?
+						filename = get_defpath(single_match)
+					else
+						puts "Several matches in repository. Choose:"
+						entries.each_with_index { |item, i| puts "#{i+1}. #{item}" }
+						filename = get_defpath(
+							entries[get_user_number_in_range((1..entries.size))-1]
+						)
+					end
+				end
+			end
+		
+			parse_def(filename)
+		end
+		
+		def write_n_diff(stub)
+			dest = stub[:destination]
+		
+			generated_file_path = get_generated_file_path(dest[:filetype], dest[:filename])
+			workcopy_file_path = get_workcopy_file_path(dest[:filetype], dest[:filename])
+		
+			File.open("#{generated_file_path}", 'w') {|f| f.write(stub[:content])}
+			print "stub #{get_destination_language_from_filetype(dest[:filetype])}/#{File.basename(generated_file_path)} generated. diff with workcopy? [Yn] "
+			system("#{ENV["EDITOR"]} -d #{generated_file_path} #{workcopy_file_path}") if user_say_yes?
+		end
+		
+		def get_destination_language_from_filetype(destination_filetype)
+			case destination_filetype
+			when "rb" then "Ruby"
+			when "sc" then "SuperCollider"
+			else
+				raise "unsupported file type"
+			end
+		end
+		
+		def get_generated_file_path(destination_filetype, destination_filename)
+			dir = File.join(Dir.tmpdir, destination_filetype)
+			Dir.mkdir(dir) unless File.directory? dir
+			File.join(dir, destination_filename)
+		end
+		
+		def get_workcopy_file_path(destination_filetype, destination_filename)
+			File.join(get_workcopy_folder_path(destination_filetype), destination_filename)
+		end
+		
+		def get_workcopy_folder_path(destination_filetype)
+			path = File.join(get_rootdir, destination_filetype)
+			if File.directory?(path)
+				path
+			elsif File.file?(path)
+				path_in_file = IO.read(path).chomp
+				if File.directory?(path_in_file)
+					path_in_file
+				end
+			else
+				raise "File #{path} does not exist"
+			end or raise "Unable to determine work directory"
+		end
+		
+		def get_rb_and_sc_workcopy_file_paths_from_repo(repo_entry, rest_of_repo)
+			rb_path = get_workcopy_file_path_from_repo(repo_entry, rest_of_repo, :rb_stub)
+			sc_path = get_workcopy_file_path_from_repo(repo_entry, rest_of_repo, :sc_stub)
+			[rb_path, sc_path]
+		end
+		
+		def get_workcopy_file_path_from_repo(repo_entry, rest_of_repo, dest_type)
+			stub = generate(repo_entry, rest_of_repo, dest_type)
+			dest = stub[:destination]
+			get_workcopy_file_path(dest[:filetype], dest[:filename])
+		end
+		
+		def user_say_yes?
+			["y", ""].include?(STDIN.gets.chomp.downcase)
+		end
 
-def grrr_meta_generate(repo_entry, rest_of_repo, dest_types)
-	dest_types.each do |dest_type|
-		stub = generate(repo_entry, rest_of_repo, dest_type)
-		grrr_meta_write_n_diff(stub)
+		def get_user_number_in_range(range)
+			print "[#{range.min}-#{range.max}] "
+			number = STDIN.gets.chomp.to_i
+			exit unless range.include? number 
+			number
+		end
 	end
-end
-
-def grrr_meta_stubedit_rb(repo_entry, rest_of_repo)
-	grrr_meta_stubedit_one(repo_entry, rest_of_repo, :rb_stub)
-end
-
-def grrr_meta_stubedit_sc(repo_entry, rest_of_repo)
-	grrr_meta_stubedit_one(repo_entry, rest_of_repo, :sc_stub)
-end
-
-def grrr_meta_stubedit_one(repo_entry, rest_of_repo, dest_type)
-	relpath = grrr_meta_get_relpath(repo_entry, rest_of_repo, dest_type)
-	print "\"#{ENV["EDITOR"]} #{File.basename(relpath)}\"? [Yn] "
-	system("#{ENV["EDITOR"]} #{grrr_meta_get_rootdir}/#{relpath}") if grrr_meta_user_say_yes?
-end
-
-def grrr_meta_stubedit_both(repo_entry, rest_of_repo)
-	rb_relpath, sc_relpath = grrr_meta_get_rb_and_sc_relpaths(repo_entry, rest_of_repo)
-	print "\"#{ENV["EDITOR"]} #{[rb_relpath, sc_relpath].map {|p|File.basename(p)}.join(" ")}\"? [Yn] "
-	system("#{ENV["EDITOR"]} #{[rb_relpath, sc_relpath].map {|p|"#{grrr_meta_get_rootdir}/#{p}"}.join(" ")}") if grrr_meta_user_say_yes?
-end
-
-def grrr_meta_stubedit_sidebyside(repo_entry, rest_of_repo)
-	rb_relpath, sc_relpath = grrr_meta_get_rb_and_sc_relpaths(repo_entry, rest_of_repo)
-	print "\"#{ENV["EDITOR"]} -O2 #{[rb_relpath, sc_relpath].map {|p|File.basename(p)}.join(" ")}\"? [Yn] "
-	system("#{ENV["EDITOR"]} -O2 #{[rb_relpath, sc_relpath].map {|p|"#{grrr_meta_get_rootdir}/#{p}"}.join(" ")}") if grrr_meta_user_say_yes?
-end
-
-def grrr_meta_defedit(repo_entry)
-	defpath = grrr_meta_get_defpath(repo_entry["source"]["name"])
-	print "\"#{ENV["EDITOR"]} #{File.basename(defpath)}\"? [Yn] "
-	system("#{ENV["EDITOR"]} #{defpath}") if grrr_meta_user_say_yes?
-end
-
-def grrr_ensure_editor_environment_variable_is_set
-	raise "EDITOR environment variable is not set" unless ENV["EDITOR"]
 end
 
 if $0 == __FILE__
-	grrr_ensure_editor_environment_variable_is_set
-	
 	if ARGV.size == 0
-		operations = [
-			"generate",
-			"generate-rb",
-			"generate-sc",
-			"stubedit-rb",
-			"stubedit-sc",
-			"stubedit-both",
-			"stubedit-sidebyside",
-			"defedit"
-		]
-		puts "usage: ruby #{__FILE__} operation [defname]"
-		puts "	operation:"
-		puts operations.map { |operation| "\t\t#{operation}" }.join("\n")
+		GrrrMeta.print_usage
 	else
 		operation = ARGV[0]
 		defname = ARGV[1]
-		
-		repo_entry = grrr_meta_parse_def_arg(defname)
-		rest_of_repo = []
-		
-		case operation
-		when "generate"
-			grrr_meta_generate(repo_entry, rest_of_repo, [:rb_stub, :sc_stub])
-		when "generate-rb"
-			grrr_meta_generate(repo_entry, rest_of_repo, [:rb_stub])
-		when "generate-sc"
-			grrr_meta_generate(repo_entry, rest_of_repo, [:sc_stub])
-		when "stubedit-rb"
-			grrr_meta_stubedit_rb(repo_entry, rest_of_repo)
-		when "stubedit-sc"
-			grrr_meta_stubedit_sc(repo_entry, rest_of_repo)
-		when "stubedit-both"
-			grrr_meta_stubedit_both(repo_entry, rest_of_repo)
-		when "stubedit-sidebyside"
-			grrr_meta_stubedit_sidebyside(repo_entry, rest_of_repo)
-		when "defedit"
-			grrr_meta_defedit(repo_entry)
-		else
-			raise "unknown operation #{operation}"
-		end
+		GrrrMeta.execute(operation, defname)
 	end
 end
