2b15d7a5350406f73fe7c5bd5b490ce24d8dccd7
Anton Hörnquist <anton.hornquist@gmail.com>
2015-03-17 12:22:25 +0100

    Added shell scripts for required libraries and workcopy folders

---
+++ required_libraries.sh
@@ -0,0 +1,2 @@
+export JAH_RSCLASS_RB_DEV_DIR="/home/jah/Projects/rsclass-rb"
+export RUBYOPT="$RUBYOPT -I$JAH_RSCLASS_RB_DEV_DIR/lib"
---
+++ workcopy_folder_environment_variables.sh
@@ -0,0 +1,2 @@
+export GRRR_META_RB_DESTINATION_FOLDER="/home/jah/Projects/grrr-rb/lib/grrr"
+export GRRR_META_SC_DESTINATION_FOLDER="/home/jah/Projects/Grrr-sc/Core"
