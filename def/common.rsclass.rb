fc017694a65daa004d0db0ad37cb3933bc413042
Anton Hörnquist <anton.hornquist@gmail.com>
2017-01-17 23:12:49 +0100

    fixed ruby-only method in common

--- def/common.rsclass.rb
+++ def/common.rsclass.rb
@@ -4,4 +4,4 @@ has_class_attribute :indicate_added_removed_attached_detached, :initial_value =>
 has_class_attribute :trace_button_events, :initial_value => false
 has_class_attribute :trace_led_events, :initial_value => false
 
-has_class_method :validate_using_jruby, :arguments => [:point]
+has_class_method :validate_using_jruby, :arguments => [:point], :only_in_rb => true
