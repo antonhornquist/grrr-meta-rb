d0ff2b3b77cc0111e67737a99fd205f567b9b59b
Anton Hörnquist <anton.hornquist@gmail.com>
2017-07-10 22:12:02 +0200

    rename abstract_monome to monome_app

--- def/monome_256_app.rsclass.rb
+++ def/monome_256_app.rsclass.rb
@@ -1,3 +1,3 @@
-is_subclass_of :abstract_monome
+is_subclass_of :monome_app
 belongs_to_module :grrr, :sc_abbreviation => "GR"
 initializes_with :arguments => [:name, {:view=>nil}, {:origin=>nil}, {:create_top_view_if_none_is_supplied=>true}]
